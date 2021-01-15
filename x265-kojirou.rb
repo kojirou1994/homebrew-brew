e825393940a11d77f81592454438d6fd898aeab7
kojirou <kojirouhtc@gmail.com>
2021-01-15 15:28:33 +0800

    init

---
+++ x265-kojirou.rb
@@ -0,0 +1,66 @@
+class X265Kojirou < Formula
+  desc "H.265/HEVC encoder"
+  homepage "https://bitbucket.org/multicoreware/x265_git"
+  license "GPL-2.0-only"
+  revision 1
+  head "https://bitbucket.org/multicoreware/x265_git.git", :tag => "3.4"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "cmake" => :build
+  depends_on "nasm" => :build if Hardware::CPU.intel?
+
+  def install
+    # Build based off the script at ./build/linux/multilib.sh
+    args = std_cmake_args + %W[
+      -DLINKED_10BIT=ON
+      -DLINKED_12BIT=ON
+      -DEXTRA_LINK_FLAGS=-L.
+      -DEXTRA_LIB=x265_main10.a;x265_main12.a
+      -DCMAKE_INSTALL_RPATH=#{lib}
+    ]
+    high_bit_depth_args = std_cmake_args + %w[
+      -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF
+      -DENABLE_SHARED=OFF -DENABLE_CLI=OFF
+    ]
+    (buildpath/"8bit").mkpath
+
+    mkdir "10bit" do
+      system "cmake", buildpath/"source", "-DENABLE_HDR10_PLUS=ON", *high_bit_depth_args
+      system "make"
+      mv "libx265.a", buildpath/"8bit/libx265_main10.a"
+    end
+
+    mkdir "12bit" do
+      system "cmake", buildpath/"source", "-DMAIN12=ON", *high_bit_depth_args
+      system "make"
+      mv "libx265.a", buildpath/"8bit/libx265_main12.a"
+    end
+
+    cd "8bit" do
+      system "cmake", buildpath/"source", *args
+      system "make"
+      mv "libx265.a", "libx265_main.a"
+     if OS.mac?
+      system "libtool", "-static", "-o", "libx265.a", "libx265_main.a",
+                        "libx265_main10.a", "libx265_main12.a"
+     else
+      system "ar", "cr", "libx265.a", "libx265_main.a", "libx265_main10.a",
+                         "libx265_main12.a"
+      system "ranlib", "libx265.a"
+     end
+      system "make", "install"
+    end
+  end
+
+  test do
+    yuv_path = testpath/"raw.yuv"
+    x265_path = testpath/"x265.265"
+    yuv_path.binwrite "\xCO\xFF\xEE" * 3200
+    system bin/"x265", "--input-res", "80x80", "--fps", "1", yuv_path, x265_path
+    header = "AAAAAUABDAH//w=="
+    assert_equal header.unpack("m"), [x265_path.read(10)]
+  end
+end
